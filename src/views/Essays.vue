e12d4db1f8661ca105d0aac5db5be63a76bd64fd
Jim Fowler <fowler@math.osu.edu>
2019-12-30 09:37:31 -0500

    Better essays

--- src/views/Essays.vue
+++ src/views/Essays.vue
@@ -2,8 +2,8 @@
 <v-form :key="key">
   <v-container fluid>
     <v-row><v-col><v-card>
-    <v-card-title>An interesting mathematical problem</v-card-title>
-    <v-card-subtitle>What is an interesting mathematical problem you have worked on?</v-card-subtitle>
+    <v-card-title>Problems</v-card-title>
+    <v-card-subtitle><strong>What is an interesting mathematical problem you have worked on?</strong> Recall a problem that you spent some time thinking about. Carefully state the problem. Describe the work you have done on it.</v-card-subtitle>
     <v-card-text class="py-0">
       <v-textarea
 	outlined
@@ -25,8 +25,8 @@
   </v-card></v-col></v-row>
 
   <v-row><v-col><v-card>
-    <v-card-title>An interesting project</v-card-title>
-    <v-card-subtitle>Have you worked on an interesting mathematical project?  If so, how was your experience?</v-card-subtitle>
+    <v-card-title>Projects</v-card-title>
+    <v-card-subtitle><strong>Have you worked on some interesting mathematical projects?</strong> If so, what were the topics? For the project you enjoyed the most: How much time did you spend on it? Were you working alone, guided by a mentor, or as part of a team? What benefits do you feel you gained from doing that work?</v-card-subtitle>
     <v-card-text class="py-0">
       <v-textarea
 	outlined
@@ -49,7 +49,7 @@
 
   <v-row><v-col><v-card>
     <v-card-title>Competitions</v-card-title>
-    <v-card-subtitle>Have you participated in mathematical contests or competitions?  How were your experiences?</v-card-subtitle>
+    <v-card-subtitle><strong>Have you recently participated in some math competitions?</strong> Which ones? Did you do well on them? How do your math contest experiences (both preparing for and participating in contests), compare with math courses you have taken in high school or in other venues?</v-card-subtitle>
     <v-card-text class="py-0">
       <v-textarea
 	outlined
@@ -72,7 +72,7 @@
 
   <v-row><v-col><v-card>
     <v-card-title>Other programs</v-card-title>
-    <v-card-subtitle>Have you participated in other mathematics programs, such as a summer mathcamp or a "math circle" meeting after school?  If so, what programs have you participated in?</v-card-subtitle>
+    <v-card-subtitle><strong>Have you participated in academic programs outside of school?</strong> This might include a local Math Club or a Math Circle at some nearby college.  What sorts of activities were involved? Did you enjoy those experiences?</v-card-subtitle>
     <v-card-text class="py-0">
       <v-textarea
 	outlined
@@ -95,7 +95,7 @@
 
   <v-row><v-col><v-card>
     <v-card-title>Books and websites</v-card-title>
-    <v-card-subtitle>What are some of your favorite mathematical books?  Your favorite mathematics websites?</v-card-subtitle>
+    <v-card-subtitle><strong>What have you read recently about mathematics?</strong> Which math books are your favorites? What parts of them were most enjoyable or interesting to you? What interesting mathematical websites have you visited in the past year? What aspects of math have you learned by reading the material on those sites?</v-card-subtitle>
     <v-card-text class="py-0">
       <v-textarea
 	outlined
@@ -117,8 +117,8 @@
   </v-card></v-col></v-row>
 
   <v-row><v-col><v-card>
-    <v-card-title>Intended major and career</v-card-title>
-    <v-card-subtitle>When you attend college, what do you intend to major in?  What are your career goals?</v-card-subtitle>
+    <v-card-title>Future goals</v-card-title>
+    <v-card-subtitle><strong>What do you plan to major in at college?</strong> What are your career goals? Do you feel &ldquo;driven&rdquo; toward one type of work? (Like an academic career in math, astronomy, or economics? Or a career in finance? etc.) Or will you take a variety of courses in college and see what areas seem most interesting?</v-card-subtitle>
     <v-card-text class="py-0">
       <v-textarea
 	outlined
@@ -141,7 +141,7 @@
 
   <v-row><v-col><v-card>
     <v-card-title>Collaboration</v-card-title>
-    <v-card-subtitle>Do you normally work on mathematics alone?  Do you have experience working in groups?</v-card-subtitle>
+    <v-card-subtitle><strong>When you work on hard math problems do you usually work alone? Or with a group of friends?</strong>  Reminder: when you work on the Ross Application Problems, please be sure to work by yourself.</v-card-subtitle>
     <v-card-text class="py-0">
       <v-textarea
 	outlined
@@ -163,8 +163,8 @@
   </v-card></v-col></v-row>
 
   <v-row><v-col><v-card>
-    <v-card-title>Other courses</v-card-title>
-    <v-card-subtitle>What previous coursework have you pursued in mathematics?  Have you taken courses (either online or face-to-face) outside of your high school?</v-card-subtitle>
+    <v-card-title>Other coursework</v-card-title>
+    <v-card-subtitle><strong>Have you taken math courses outside of the standard high school curriculum?</strong> Those courses could have been at a local college, in a residential math program, from an educational website, etc.</v-card-subtitle>
     <v-card-text class="py-0">
       <v-textarea
 	outlined
@@ -187,7 +187,7 @@
 
   <v-row><v-col><v-card>
     <v-card-title>Being away from home</v-card-title>
-    <v-card-subtitle>Are you eager to spend many weeks away from home, away from television and movies and computers and video games, away from family, spending all available time on mathematical explorations?</v-card-subtitle>
+    <v-card-subtitle><strong>Are you eager to spend five or six weeks away from home, with no distractions from computers or video games or smart phones, focusing all of your energies on one narrow area of mathematics?</strong> Ross students live in a college dormitory, with no access to televisions, computers, or electronic games. Most of their waking hours are spent working on challenging math problems. Does this intensity and focus appeal to you?</v-card-subtitle>
     <v-card-text class="py-0">
       <v-textarea
 	outlined
